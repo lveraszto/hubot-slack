9d41a3b1fff39e5bbfd72dc59b26655552c6904d
Laszlo Veraszto <laszlo.veraszto@emarsys.com>
2023-08-11 13:19:00 +0200

    AA-17213: RTMClient hack to add null check and avoid error, increase version

--- src/client.js
+++ src/client.js
@@ -2,6 +2,7 @@
 
 const { RTMClient } = require('@slack/rtm-api');
 const { WebClient } = require('@slack/web-api');
+const errors_1 = require('@slack/rtm-api/dist/errors');
 
 class SlackClient {
   static #CONVERSATION_CACHE_TTL_MS;
@@ -30,6 +31,37 @@ class SlackClient {
      * @private
      */
     SlackClient.#CONVERSATION_CACHE_TTL_MS = parseInt(process.env.HUBOT_SLACK_CONVERSATION_CACHE_TTL_MS, 10) || 5 * 60 * 1000;
+
+    /////////////RTMClient hack
+    RTMClient.prototype.send = function(type, body = {}) {
+      const message = Object.assign(Object.assign({}, body), { type, id: this.nextMessageId() });
+      return new Promise((resolve, reject) => {
+        this.logger.debug(`send() in state: ${this.stateMachine.getStateHierarchy()}`);
+        if (this.websocket === undefined) {
+          this.logger.error('cannot send message when client is not connected');
+          reject((0, errors_1.sendWhileDisconnectedError)());
+        }
+        else if (!(this.stateMachine.getCurrentState() === 'connected' &&
+          this.stateMachine.getStateHierarchy()[1] === 'ready')) {
+          this.logger.error('cannot send message when client is not ready');
+          reject((0, errors_1.sendWhileNotReadyError)());
+        }
+        else {
+          // NOTE: future feature request: middleware pipeline to process the message before its sent
+          this.emit('outgoing_message', message);
+          const flatMessage = JSON.stringify(message);
+          this.logger.debug(`sending message on websocket: ${flatMessage}`);
+          this.websocket.send(flatMessage, (error) => {
+            if (error) {
+              this.logger.error(`failed to send message on websocket: ${error.message}`);
+              return reject((0, errors_1.websocketErrorWithOriginal)(error));
+            }
+            return resolve(message.id);
+          });
+        }
+      });
+    };
+    /////////////
 
     // Client initialization
     // NOTE: the recommended initialization options are `{ dataStore: false, useRtmConnect: true }`. However the
